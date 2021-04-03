ce8b2ab235e9a0fb71908feb8c6a619a7374729f
jakublaba <jakub.maciej.laba@gmail.com>
2021-04-03 16:34:16 +0200

    wstępne sterowanie wężem

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
 
-public class GameFrame{
-
-
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        super("Snake GameFrame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 800);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.add(new MousePanel());
+    }
 }
--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -1,8 +1,5 @@
 public class SnakeMain {
     public static void main(String[] args) {
-        GameBoard g = new GameBoard(100, 100);
-        for(int i = 0; i < 10; i++) {
-            g.respawnFood();
-        }
+        new GameFrame();
     }
 }
