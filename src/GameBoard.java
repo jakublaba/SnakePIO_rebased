881a693632cd2fd47490630d7ac1a93d93bf8295
patrykferenc <patrykferenc@protonmail.com>
2021-04-11 18:33:54 +0200

    Snake zamieniony z LinkedList na ArrayList - tak jak dr Olszewski przykazał

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,56 +1,57 @@
 import java.awt.geom.Point2D;
-import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GameBoard {
     public int boardHeight, boardWidth;
     public static Snake snake;
-    public LinkedList<Point2D.Double> obstacles;
     public Point2D.Double food;
 
     public GameBoard(int boardHeight, int boardWidth, int segmentSize) {
         this.boardHeight = boardHeight;
         this.boardWidth = boardWidth;
-        this.food = new Point2D.Double(ThreadLocalRandom.current().nextDouble(segmentSize/2, boardWidth - segmentSize/2), ThreadLocalRandom.current().nextDouble(segmentSize/2, boardHeight - segmentSize/2));
+        this.food = new Point2D.Double(ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardWidth - segmentSize / 2),
+                ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardHeight - segmentSize / 2));
     }
 
     void respawnFood(int segmentSize) {
-        Point2D.Double newFood = new Point2D.Double(ThreadLocalRandom.current().nextDouble(segmentSize/2, boardWidth - segmentSize/2), ThreadLocalRandom.current().nextDouble(segmentSize/2, boardHeight - segmentSize/2));
-        newFood.setLocation(ThreadLocalRandom.current().nextDouble(segmentSize/2, boardWidth - segmentSize/2), ThreadLocalRandom.current().nextDouble(segmentSize/2, boardHeight - segmentSize/2));
+        Point2D.Double newFood = new Point2D.Double(ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardWidth - segmentSize / 2),
+                ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardHeight - segmentSize / 2));
+        newFood.setLocation(ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardWidth - segmentSize / 2),
+                ThreadLocalRandom.current().nextDouble(segmentSize / 2, boardHeight - segmentSize / 2));
         food = newFood;
     }
 
     public void checkBorderCollision(double gameSegmentSize) {
-        double distFromTop = snake.bodySegments.getFirst().getY();
+        double distFromTop = snake.bodySegments.get(0).getY();
         double distFromBottom = boardHeight - distFromTop;
-        double distFromLeft = snake.bodySegments.getFirst().getX();
+        double distFromLeft = snake.bodySegments.get(0).getX();
         double distFromRight = boardWidth - distFromLeft;
-        if(distFromTop <= gameSegmentSize/2) {
+        if (distFromTop <= gameSegmentSize / 2) {
             System.out.println("Game Over: collision with top border");
-            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.getFirst().getX(), snake.bodySegments.getFirst().getY());
+            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.get(0).getX(), snake.bodySegments.get(0).getY());
             System.exit(1);
         }
-        if(distFromBottom <= gameSegmentSize/2) {
+        if (distFromBottom <= gameSegmentSize / 2) {
             System.out.println("Game Over: collision with bottom border");
-            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.getFirst().getX(), snake.bodySegments.getFirst().getY());
+            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.get(0).getX(), snake.bodySegments.get(0).getY());
             System.exit(1);
         }
-        if(distFromLeft <= gameSegmentSize/2) {
+        if (distFromLeft <= gameSegmentSize / 2) {
             System.out.println("Game Over: collision with left border");
-            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.getFirst().getX(), snake.bodySegments.getFirst().getY());
+            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.get(0).getX(), snake.bodySegments.get(0).getY());
             System.exit(1);
         }
-        if(distFromRight <= gameSegmentSize/2) {
+        if (distFromRight <= gameSegmentSize / 2) {
             System.out.println("Game Over: collision with right border");
-            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.getFirst().getX(), snake.bodySegments.getFirst().getY());
+            System.out.printf("Coordinates of head when game ended: (%f, %f)\n", snake.bodySegments.get(0).getX(), snake.bodySegments.get(0).getY());
             System.exit(1);
         }
     }
 
     public void checkTailCollision(double gameSegmentSize) {
-        if(snake.bodySegments.size() > 10) { //magic number bruh
-            for(int i = 10; i < snake.bodySegments.size(); i++) { //BRUH
-                if(Math.abs(snake.bodySegments.getFirst().distance(snake.bodySegments.get(i))) < gameSegmentSize/2) {
+        if (snake.bodySegments.size() > 7) { //magic number bruh
+            for (int i = 7; i < snake.bodySegments.size(); i++) { //BRUH
+                if (Math.abs(snake.bodySegments.get(0).distance(snake.bodySegments.get(i))) < gameSegmentSize / 2) {
                     System.out.printf("Game Over: collision with tail segment number %d\n", i);
                     System.exit(1);
                 }
@@ -59,10 +60,10 @@ public class GameBoard {
     }
 
     public boolean checkFood(double gameSegmentSize) {
-        double xDiffSqr = (snake.bodySegments.getFirst().getX()-food.getX())*(snake.bodySegments.getFirst().getX()-food.getX());
-        double yDiffSqr = (snake.bodySegments.getFirst().getY()-food.getY())*(snake.bodySegments.getFirst().getY()-food.getY());
+        double xDiffSqr = (snake.bodySegments.get(0).getX() - food.getX()) * (snake.bodySegments.get(0).getX() - food.getX());
+        double yDiffSqr = (snake.bodySegments.get(0).getY() - food.getY()) * (snake.bodySegments.get(0).getY() - food.getY());
         double distance = Math.sqrt(xDiffSqr + yDiffSqr);
-        return distance < gameSegmentSize/2;
+        return distance < gameSegmentSize / 2;
     }
 
 }
--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -20,20 +20,23 @@ public class MenuFrame extends JFrame implements ActionListener {
         this.myButton();
 
     }
-    public void myBorderPanel(){
+
+    public void myBorderPanel() {
         JLabel tekst = new JLabel("Snake");
-        tekst.setFont(new Font("Courier New", (Font.BOLD),20));
-        tekst.setBounds(365,200,100,100);
+        tekst.setFont(new Font("Courier New", (Font.BOLD), 20));
+        tekst.setBounds(365, 200, 100, 100);
         panel.add(tekst);
     }
+
     JButton button;
     JButton button1;
-    public void myButton(){
+
+    public void myButton() {
         button = new JButton("New Game");
-        button.setBounds(300,300,200,50);
+        button.setBounds(300, 300, 200, 50);
         panel.add(button);
         button1 = new JButton("Exit");
-        button1.setBounds(300,400,200,50);
+        button1.setBounds(300, 400, 200, 50);
         panel.add(button1);
         button.addActionListener(this);
         button1.addActionListener(this);
@@ -42,11 +45,11 @@ public class MenuFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if(source == button) {
+        if (source == button) {
             setVisible(false);
             new GameFrame();
         }
-        if(source == button1) {
+        if (source == button1) {
             System.exit(0);
         }
     }
