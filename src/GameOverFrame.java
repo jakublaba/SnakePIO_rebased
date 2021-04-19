f57a638065ceb483c82aef57e333817099163b1c
jakublaba <jakub.maciej.laba@gmail.com>
2021-04-19 13:13:42 +0200

    bsdug

---
+++ src/GameOverFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameOverFrame extends JFrame{
+    public GameOverFrame() {
+        super("Snake");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        panel.setBackground(new Color(3, 192, 60));
+        panel.setPreferredSize(new Dimension(800, 800));
+        JLabel message = new JLabel("Game Over");
+        panel.add(message);
+        message.setFont(new Font("Arial", Font.PLAIN, 32));
+        this.setContentPane(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setMinimumSize(this.getSize());
+        this.setVisible(true);
+        this.setResizable(false);
+    }
+}
