281734eb78a6964801b2863959718983b8e7a01a
patrykferenc <patrykferenc@protonmail.com>
2021-04-21 08:16:35 +0200

    pierwsza wersja, fragment implementacji

---
+++ src/Obstacle.java
@@ -0,0 +1,28 @@
+import java.awt.geom.Point2D;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Obstacle {
+    private final int safeHeight, safeWidth, segmentSize;
+    private Point2D.Double position;
+
+    public Obstacle(int width, int height, int segment) {
+        this.segmentSize = segment;
+        this.safeWidth = width - this.segmentSize;
+        this.safeHeight = height - this.segmentSize;
+        this.position = new Point2D.Double(ThreadLocalRandom.current().nextDouble(this.segmentSize, safeWidth),
+                ThreadLocalRandom.current().nextDouble(this.segmentSize, safeHeight));
+    }
+
+    public double getX() {
+        return position.getX();
+    }
+
+    public double getY() {
+        return position.getY();
+    }
+
+    public void respawn() {
+        this.position.setLocation(ThreadLocalRandom.current().nextDouble(this.segmentSize, this.safeWidth),
+                ThreadLocalRandom.current().nextDouble(this.segmentSize, this.safeHeight));
+    }
+}
